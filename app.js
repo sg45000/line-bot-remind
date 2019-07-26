c03e5ae8d9e36a6540ff363455b9eb196720db4b
sg45000 <hamakyo711@gmail.com>
2019-07-26 22:35:17 +0900

    スケジュールジョブの時間変更3

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json());
     }
 
     function registerScheduleJob(){
-        let job = schedule.scheduleJob("0 27 22 * * *", (firedata)=>{
+        let job = schedule.scheduleJob("0 39 22 * * *", (firedata)=>{
                 getUserInfo()
                     .then(all_users=> {
                         console.log(all_users);
