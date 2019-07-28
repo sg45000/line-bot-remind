704a8a2cf8cb0d966280366e7381aca56be13f6c
sg45000 <hamakyo711@gmail.com>
2019-07-28 15:06:18 +0900

    実行時間とメッセージタイトル変更

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json());
     }
 
     function registerScheduleJob(){
-        let job = schedule.scheduleJob('0 17 0 * * *', (firedata)=>{
+        let job = schedule.scheduleJob('0 0 21 * * *', (firedata)=>{
                 getUserInfo()
                     .then(all_users=> {
                         console.log(all_users);
